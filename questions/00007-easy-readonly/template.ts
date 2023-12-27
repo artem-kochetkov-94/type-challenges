6a351a48cb7d3d02af37dd43d6603862fe609795
artem <artem-kochetkov-94@mail.ru>
2023-12-27 23:02:49 +0500

    00011

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,7 +1,7 @@
 type MyReadonly<T> = {
-    readonly [P in (keyof T)]: T[P];
+    readonly [P in (keyof T)]: T[P]
 }
 
 type MyDeepReadonly<T> = {
-    readonly [P in (keyof T)]: T[P] extends {} ? MyReadonly<T[P]> : T[P];
+    readonly [P in (keyof T)]: T[P] extends {} ? MyReadonly<T[P]> : T[P]
 }

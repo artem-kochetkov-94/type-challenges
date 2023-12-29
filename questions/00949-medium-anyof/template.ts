4c7fb9291503026fbbd95afc3d59cf58120f6790
artem <artem-kochetkov-94@mail.ru>
2023-12-29 22:49:56 +0500

    anyof

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,8 @@
-type AnyOf<T extends readonly any[]> = any
+type Falsy = 0 | '' |
+ false | null | undefined | [] | { [P in any]: never }
+
+type AnyOf<T extends readonly any[]> = T extends [infer First, ...infer Rest]
+    ? First extends Falsy
+    ? AnyOf<Rest>
+    : true
+    : false

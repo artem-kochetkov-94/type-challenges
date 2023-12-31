895065b7f50b22c5b4d1a1ea84b7a10f64193979
artem <artem-kochetkov-94@mail.ru>
2023-12-31 14:12:19 +0500

    trim update

--- questions/00106-medium-trimleft/template.ts
+++ questions/00106-medium-trimleft/template.ts
@@ -1,5 +1,6 @@
 type SpaceLeft = ' ' | '\n' | '\t'
 
-type TrimLeft<S extends string> = S extends `${SpaceLeft}${infer R}`
-  ? TrimLeft<R>
-  : S
+type TrimLeft<S extends string> =
+  S extends `${SpaceLeft}${infer R}`
+    ? TrimLeft<R>
+    : S
--- questions/00108-medium-trim/template.ts
+++ questions/00108-medium-trim/template.ts
@@ -6,3 +6,11 @@ type Trim<S extends string> =
     : S extends `${infer M2}${Space}`
       ? Trim<M2>
       : S
+
+
+type Trim2<S extends string> =
+  S extends
+    (`${Space}${infer M}`
+    | `${infer M}${Space}`)
+      ? Trim<M>
+      : S

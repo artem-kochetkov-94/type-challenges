feb854838a2a3b12bdec077addd5a12c5998c45d
artem <artem-kochetkov-94@mail.ru>
2023-12-31 18:33:04 +0500

    percentage-parser

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,19 @@
-type PercentageParser<A extends string> = any
+type ParseSign<T> =
+  T extends `${infer Sign extends '+' | '-'}${infer _Tail}`
+    ? Sign
+    : ''
+
+type ParseNumber<T> = T extends `${ParseSign<T>}${infer N}${ParsePercent<T>}`
+  ? N
+  : ''
+
+type ParsePercent<T> = T extends `${string}${infer Tail extends '%'}`
+  ? Tail
+  : ''
+
+type PercentageParser<T extends string> = 
+  [
+    ParseSign<T>,
+    ParseNumber<T>,
+    ParsePercent<T>,
+  ]

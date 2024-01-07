f91e5659c631c9848ebf7da0cac42119458d8334
artem <artem-kochetkov-94@mail.ru>
2024-01-07 21:15:33 +0500

    integer, construct-tuple

--- questions/07544-medium-construct-tuple/template.ts
+++ questions/07544-medium-construct-tuple/template.ts
@@ -1,1 +1,7 @@
-type ConstructTuple<L extends number> = any
+type ConstructTuple<
+  L extends number,
+  Acc extends unknown[] = [],
+> =
+  Acc['length'] extends L
+    ? Acc
+    : ConstructTuple<L, [...Acc, unknown]>
--- questions/10969-medium-integer/template.ts
+++ questions/10969-medium-integer/template.ts
@@ -1,1 +1,22 @@
-type Integer<T> = any
+type Integer<T> =
+  number extends T
+    ? never
+    : T extends number
+      ? `${T}` extends `${infer Base}.${infer Tail}`
+        ? IsZeroString<Tail> extends true
+          ? Base
+          : never
+        : T
+      : never
+
+type IsZeroString<T> =
+  T extends `${infer Head}${infer Tail}`
+    ? Head extends '0'
+      ? IsZeroString<Tail>
+      : false
+    : true
+
+type Integer2<T> =
+  `${T & number}` extends `${string}.${string}`
+    ? never
+    : number extends T ? never : T
